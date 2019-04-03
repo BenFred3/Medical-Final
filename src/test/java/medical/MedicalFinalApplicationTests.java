c7fe5be20c03b39eed2d261e51e5c8f3c7fb8cda
rmfrasch <44025964+rmfrasch@users.noreply.github.com>
2019-04-02 22:34:32 -0500

    intial commit

---
+++ src/test/java/medical/MedicalFinalApplicationTests.java
@@ -0,0 +1,16 @@
+package medical;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MedicalFinalApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
