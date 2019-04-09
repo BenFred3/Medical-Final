f326e0201690e54f7a8d55e47b8a8eb3e08701a4
rmfrasch <44025964+rmfrasch@users.noreply.github.com>
2019-04-09 01:01:30 -0500

    I added the ability to view all appointments for a given patient.  I was having issues figuring how to only see an individual's appointments, I ended up adding a method to the appointment repo interface.

--- src/main/java/medical/repository/AppointmentRepository.java
+++ src/main/java/medical/repository/AppointmentRepository.java
@@ -1,9 +1,16 @@
 // Package and import statements.
 package medical.repository;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import medical.beans.Appointments;
 
 
 @Repository
-public interface AppointmentRepository extends JpaRepository<Appointments, Long> { }
+public interface AppointmentRepository extends JpaRepository<Appointments, Long> { 
+	
+	List<Appointments> findByPatientId(long patientId);
+}
+
